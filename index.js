f0c87c6b62c6c2dd3b2d1a10e02a77effe33c6e9
juliachan <54juliachan@gmail.com>
2025-12-08 09:44:40 +0800

    firebaseKey搬到.env

--- index.js
+++ index.js
@@ -4,20 +4,6 @@ import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.6.0/firebase
 import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } 
 from "https://www.gstatic.com/firebasejs/12.6.0/firebase-auth.js";
 
-// ---------------------- Firebase 初始化 ----------------------
-const firebaseConfig = {
-  apiKey: "AIzaSyC6CUxG-B1ZkuFBk1zAGp1I2-0n3KilY6w",
-  authDomain: "lifetoon-chat.firebaseapp.com",
-  projectId: "lifetoon-chat",
-  storageBucket: "lifetoon-chat.firebasestorage.app",
-  messagingSenderId: "962298495930",
-  appId: "1:962298495930:web:388d5cd407728533955383",
-  measurementId: "G-8ZW9TZZ2XS"
-};
-
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const auth = getAuth(app);
 
 // ---------------------- DOM 事件綁定 ----------------------
 window.addEventListener("DOMContentLoaded", () => {
